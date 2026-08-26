0acd520bb81071986f8616da8f8fe064089b3969
Rapix27 <rapix24052011@gmail.com>
2026-08-26 09:15:28 -0300

    cagamos hoje

--- app.js
+++ app.js
@@ -15,45 +15,29 @@
             this.vida = "eliminado"
         }
        }
+
+   estaVivo(){
+    return this.vida > 0
+    }
+
+
+    mostrarStatus(){
+        console.log(this.nome +" | Vida: " + this.vida)
+    }
    }
 
+
    const druida = new Personagem ("Kode", 80, 25)
-  
-
 const guerreiro = new Personagem("Thorin", 60, 20)
-
 const mago = new Personagem("Gandalfe", 60, 35)
-
-const arqueiro = new Personagem("Legolas", 80, 25)
-
-
-    
+const arqueiro = new Personagem("Legolas", 80, 25)   
 const protagonista = new Personagem("Rapix", 100, 50) 
- 
 const barbaro = new Personagem("Varr", 90, 25) 
- 
 const feiticeira = new Personagem("Morgana", 70, 30) 
- 
-const ninja = new Personagem("Kaito", 80, 35) 
- 
+const ninja = new Personagem("Kaito", 80, 35)  
 const vampiro = new Personagem("Dracula", 120, 15) 
 
+druida.causaDano(mago)
+console.log(mago.estaVivo())
+console.log(mago.mostrarStatus())
 
-
-protagonista.causaDano(barbaro) 
-barbaro.causaDano(feiticeira) 
-feiticeira.causaDano(ninja) 
-ninja.causaDano(vampiro) 
-vampiro.causaDano(protagonista) 
-protagonista.causaDano(feiticeira) 
-barbaro.causaDano(vampiro) 
-ninja.causaDano(barbaro) 
-
-
-console.log(protagonista.vida) 
-console.log(barbaro.vida) 
-console.log(feiticeira.vida) 
-console.log(ninja.vida) 
-console.log(vampiro.vida)
-
-console.log("Morgana foi derrotada pelo Rapix")
--- teste.js
+++ teste.js
@@ -1,55 +1,53 @@
+   class Personagem {
+       constructor (nome, vida, ataque){
+        this.nome = nome,
+        this.vida = vida,
+        this.ataque = ataque
+       }
+
+       causaDano(personagem){
+        personagem.recebeDano(this.ataque)
+       }
+       recebeDano(quantidade){
+        this.vida = this.vida - quantidade 
+
+        if(this.vida <= 0){
+            this.vida = "eliminado"
+        }
+       }
+
+   estaVivo(){
+    return this.vida > 0
+    }
+   }
+
+
+   const druida = new Personagem ("Kode", 80, 25)
+const guerreiro = new Personagem("Thorin", 60, 20)
+const mago = new Personagem("Gandalfe", 60, 35)
+const arqueiro = new Personagem("Legolas", 80, 25)   
+const protagonista = new Personagem("Rapix", 100, 50) 
+const barbaro = new Personagem("Varr", 90, 25) 
+const feiticeira = new Personagem("Morgana", 70, 30) 
+const ninja = new Personagem("Kaito", 80, 35)  
+const vampiro = new Personagem("Dracula", 120, 15) 
+
+
+
+protagonista.causaDano(barbaro) 
+barbaro.causaDano(feiticeira) 
+feiticeira.causaDano(ninja) 
+ninja.causaDano(vampiro) 
+vampiro.causaDano(protagonista) 
+protagonista.causaDano(feiticeira) 
+barbaro.causaDano(vampiro) 
+ninja.causaDano(barbaro) 
+
+console.log(protagonista.vida) 
+console.log(barbaro.vida) 
+console.log(feiticeira.vida) 
+console.log(ninja.vida) 
+console.log(vampiro.vida)
+
+console.log("Morgana foi derrotada pelo Rapix")
    
-   const guerreiro = {
-    nome: "Thorin",
-    vida: 60,
-    ataque: 20,
-
-    causaDano: function(personagem){
-    personagem.recebeDano(this.ataque)
-    },
-    recebeDano: function(quantidade){
-    this.vida = this.vida - quantidade
-
-    if(this.vida <= 0){
-        this.vida = "eliminado"
-    }
-}
-  
-}
-    const mago = {
-    nome: "Gandalfe",
-    vida: 60,
-    ataque: 35,
-
-    recebeDano: function(quantidade){
-    this.vida = this.vida - quantidade
-
-    if(this.vida <= 0){
-        this.vida = "eliminado"
-    }
-    },
-causaDano: function(personagem){
-    personagem.recebeDano(this.ataque)
-    }
-}
-    const arqueiro = {
-    nome: "Legolas",
-    vida: 80,
-    ataque: 25,
-
-    recebeDano: function(quantidade){
-    this.vida = this.vida - quantidade
-
-    if(this.vida <= 0){
-        this.vida = "eliminado"
-    }
-    },
-
-    causaDano: function(personagem){
-    personagem.recebeDano(this.ataque)
-    }
-}
-guerreiro.causaDano(mago)
-arqueiro.causaDano(mago)
-guerreiro.causaDano(mago)
-console.log(mago.vida)
