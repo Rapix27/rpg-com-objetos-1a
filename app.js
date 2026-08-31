97b5a5e1720d841eaffe2f217dfc54c57f4e67c5
Rapix27 <rapix24052011@gmail.com>
2026-08-31 08:37:28 -0300

    Novas Regras

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
    class Personagem {
-       constructor (nome, vida, ataque){
+       constructor (nome, vida, ataque, defesa){
         this.nome = nome,
         this.vida = vida,
-        this.ataque = ataque
+        this.vidaMaxima = vida,
+        this.ataque = ataque,
+        this.defesa = defesa
+       }
+       
+       curar(quantidade){
+        this.vida = this.vida + quantidade
+
+        if (this.vida > this.vidaMaxima){
+            this.vida = this.vidaMaxima
+        } 
        }
 
        causaDano(personagem){
         personagem.recebeDano(this.ataque)
        }
+
        recebeDano(quantidade){
-        this.vida = this.vida - quantidade 
+        let dano = quantidade - this.defesa
+
+        if(quantidade < this.defesa){
+            dano = 0
+        }
+
+        this.vida = this.vida - dano
 
         if(this.vida <= 0){
             this.vida = "eliminado"
@@ -27,15 +44,15 @@
    }
 
 
-   const druida = new Personagem ("Kode", 80, 25)
-const guerreiro = new Personagem("Thorin", 60, 20)
-const mago = new Personagem("Gandalfe", 60, 35)
-const arqueiro = new Personagem("Legolas", 80, 25)   
-const protagonista = new Personagem("Rapix", 100, 50) 
-const barbaro = new Personagem("Varr", 90, 25) 
-const feiticeira = new Personagem("Morgana", 70, 30) 
-const ninja = new Personagem("Kaito", 80, 35)  
-const vampiro = new Personagem("Dracula", 120, 15) 
+const druida = new Personagem ("Kode", 70, 30, 8)
+const guerreiro = new Personagem("Thorin", 60, 20, 15)
+const mago = new Personagem("Gandalfe", 60, 35, 5)
+const arqueiro = new Personagem("Legolas", 80, 25, 12)   
+const protagonista = new Personagem("Rapix", 100, 50, 50) 
+const barbaro = new Personagem("Varr", 90, 25, 6) 
+const feiticeira = new Personagem("Morgana", 70, 30, 8) 
+const ninja = new Personagem("Kaito", 80, 35, 5)  
+const vampiro = new Personagem("Dracula", 120, 15, 5) 
 
 druida.causaDano(mago)
 console.log(mago.estaVivo())
